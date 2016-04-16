d62e46c4f2e956eb3370c00464f1f09e49c0a106
lirahalu <704867262@qq.com>
2016-04-16 21:27:09 +0800

    代码优化

--- js/show.js
+++ js/show.js
@@ -2,11 +2,10 @@ function showNumberWithAnimation(i, j, randNumber) {
 
     var numberCell = $("#number-cell-" + i + "-" + j);
 
-    numberCell.css("background-color", getNumberBackgroundColor(randNumber));
-    numberCell.css("color", getNumberColor(randNumber));
-    numberCell.text(randNumber);
-
-    numberCell.animate({
+    numberCell.css("background-color", getNumberBackgroundColor(randNumber))
+        .css("color", getNumberColor(randNumber))
+        .text(randNumber)
+        .animate({
         width: cellSideLength,
         height: cellSideLength,
         top: getPosTop(i, j),
